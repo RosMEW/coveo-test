3baca0c20de6a44f7da26541a549e17cb2f6c6f2
RosMEW <rosalie.yue.liu@gmail.com>
2019-06-21 09:54:23 -0400

    Add logo to header

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -4,7 +4,15 @@ import './Header.scss';
 
 const Header = () => (
     <header>
-        <div>header</div>
+        <div className='header__container'>
+            <img src='/img/logo-saq.png' alt='SAQ-Logo' />
+            <div className='searchbar'>
+                <div>searchbar</div>
+                <div>searchtext</div>
+            </div>
+            <div className='promotion'>En special</div>
+            <div className='price'>price</div>
+        </div>
     </header>
 );
 
