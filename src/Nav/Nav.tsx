9207037807e0352fbeba9a2890ca1c7d286cd5d2
RosMEW <rosalie.yue.liu@gmail.com>
2019-06-23 23:13:48 -0400

    Add navigation section

--- src/Nav/Nav.tsx
+++ src/Nav/Nav.tsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import NavItem from './NavItem/NavItem';
 import './Nav.scss';
 
-const Nav = () => (
-    <nav>
-        <div>nav</div>
-    </nav>
-);
+const navItems = [
+    'Catégorie',
+    'Region',
+    'Qualité',
+    'Caractéristique',
+    'Recommandation',
+    'Gustative',
+    'Disponibilité'
+];
+const Nav = () => {
+    const [showMenu, setShowMenu] = useState(false);
+    const [currentNav, setCurrentNav] = useState('');
+
+    const onNavHover = (nav: string) => {
+        setShowMenu(true);
+        setCurrentNav(nav);
+    };
+
+    const onNavLeave = () => {
+        setShowMenu(false);
+        setCurrentNav('');
+    };
+
+    return (
+        <nav onMouseLeave={onNavLeave}>
+            <div className='nav'>
+                {navItems.map(nav => (
+                    <NavItem
+                        key={nav}
+                        name={nav}
+                        onMouseEnter={() => onNavHover(nav)}
+                        isSelected={nav === currentNav}
+                    />
+                ))}
+            </div>
+            <div className={`nav-menu ${showMenu ? '' : 'hidden'}`}>
+                {currentNav}
+            </div>
+        </nav>
+    );
+};
 
 export default Nav;
