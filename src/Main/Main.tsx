a8e917b9ebecaf98f7f94e309fd30a03bdfdb053
RosMEW <rosalie.yue.liu@gmail.com>
2019-06-25 17:12:23 -0400

    Add loading spinner

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import Selection from './Selection/Selection';
 import Products from './Products/Products';
+import Loader from '../UI/Loader/Loader';
+
+import { state } from '../shared/types';
 import './Main.scss';
 
-const Main = () => (
-    <main>
-        <Selection />
-        <Products />
-    </main>
-);
+const Main = () => {
+    const loading = useSelector((state: state) => state.search.loading);
+
+    return (
+        <main>
+            {loading ? <Loader /> : <Products />} <Selection />
+        </main>
+    );
+};
 
 export default Main;
