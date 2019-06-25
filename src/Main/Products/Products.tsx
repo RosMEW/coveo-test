06f88ecb9936473e60472bbd29657d49362c714f
RosMEW <rosalie.yue.liu@gmail.com>
2019-06-25 15:34:48 -0400

    Update main section

--- src/Main/Products/Products.tsx
+++ src/Main/Products/Products.tsx
@@ -1,21 +1,41 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import Selector from '../../UI/Selector/Selector';
 import ProductCard from './ProductCard/ProductCard';
 import Pagination from '../../UI/Pagination/Pagination';
 
+import { state, product } from '../../shared/types';
+import { parseSplitGroup } from '../../helpers/utils';
 import './Products.scss';
 
+type labels = { field: string; value: string }[];
+
 const Products = () => {
-    let productCards = (
-        <React.Fragment>
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-        </React.Fragment>
-    );
+    const products = useSelector((state: state) => state.search.results);
+    const total = useSelector((state: state) => state.search.total);
+
+    const getLabels = (product: product) =>
+        [
+            {
+                field: 'tpcategorie',
+                value: product.raw.tpcategorie
+            },
+            {
+                field: 'tpformat',
+                value: product.raw.tpformat
+            },
+            {
+                field: 'tpobservationsgustativestexture',
+                value: product.raw.tpobservationsgustativestexture
+            },
+            ...parseSplitGroup(product.raw.tpcepagenomsplitgroup).map(el => ({
+                field: 'tpcepagenomsplitgroup',
+                value: el
+            }))
+        ].filter(
+            el => el.value && el.value !== 'Autre (s) cépage (s)'
+        ) as labels;
 
     return (
         <div className='products'>
@@ -23,12 +43,25 @@ const Products = () => {
                 <div className='products__header'>
                     <Selector type='sort' />
                     <div className='products__header--right'>
-                        <div className='results'>68 Articles</div>
+                        <div className='results'>{total} Articles</div>
                         <Selector type='page' />
                         <div className='results'>Résultats Par Page</div>
                     </div>
                 </div>
-                <div className='products__list'>{productCards}</div>
+                <div className='products__list'>
+                    {products.map(product => (
+                        <ProductCard
+                            key={product.uniqueId}
+                            link={product.clickUri}
+                            imgURL={product.raw.tpthumbnailuri}
+                            title={product.title}
+                            country={product.raw.tppays}
+                            region={product.raw.tpregion}
+                            price={product.raw.tpprixnum}
+                            labels={getLabels(product)}
+                        />
+                    ))}
+                </div>
                 <div className='products__footer'>
                     <Pagination />
                 </div>
--- src/Main/Selection/Selection.tsx
+++ src/Main/Selection/Selection.tsx
@@ -12,38 +12,36 @@ const Selection = () => {
     );
 
     return (
-        <div className='selection--container'>
-            <div className='selection'>
-                <div>Sélection</div>
-                <div className='selection__list'>
-                    {termSelected.map(term => (
-                        <div className='selection__item' key={term.value}>
-                            <div className='selection__item--name'>
-                                {term.text ? term.text : term.value}
-                            </div>
-                            <div
-                                className='selection__item--icon'
-                                onClick={() =>
-                                    dispatch({
-                                        type: actionTypes.REMOVE_TERM,
-                                        field: term.field,
-                                        value: term.value
-                                    })
-                                }>
-                                <img src='/svg/close.svg' alt='icon-close' />
-                            </div>
+        <div className='selection'>
+            <div>Sélection</div>
+            <div className='selection__list'>
+                {termSelected.map(term => (
+                    <div className='selection__item' key={term.value}>
+                        <div className='selection__item--name'>
+                            {term.text ? term.text : term.value}
                         </div>
-                    ))}
-                </div>
-                <div className='selection__remove-all'>
-                    Effacer Tout
-                    <div
-                        className='selection__remove-all--icon'
-                        onClick={() =>
-                            dispatch({ type: actionTypes.REMOVE_ALL_TERMS })
-                        }>
-                        <img src='/svg/spinner.svg' alt='icon-clear-all' />
+                        <div
+                            className='selection__item--icon'
+                            onClick={() =>
+                                dispatch({
+                                    type: actionTypes.REMOVE_TERM,
+                                    field: term.field,
+                                    value: term.value
+                                })
+                            }>
+                            <img src='/svg/close.svg' alt='icon-close' />
+                        </div>
                     </div>
+                ))}
+            </div>
+            <div className='selection__remove-all'>
+                Effacer Tout
+                <div
+                    className='selection__remove-all--icon'
+                    onClick={() =>
+                        dispatch({ type: actionTypes.REMOVE_ALL_TERMS })
+                    }>
+                    <img src='/svg/spinner.svg' alt='icon-clear-all' />
                 </div>
             </div>
         </div>
