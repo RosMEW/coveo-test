b9a32990254b0539237c89a91ef311337c58e809
RosMEW <rosalie.yue.liu@gmail.com>
2019-06-26 11:18:20 -0400

    Add test to main section and app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,17 +1,40 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { render, cleanup } from '@testing-library/react';
 
 import store from './store/store';
+import * as actionTypes from './store/actions/actionTypes';
 import App from './App';
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(
+const setup = () => {
+    cleanup();
+    render(
         <Provider store={store}>
             <App />
-        </Provider>,
-        div
+        </Provider>
     );
-    ReactDOM.unmountComponentAtNode(div);
+};
+
+describe('<App />', () => {
+    it('it should update selection if a term was added', async () => {
+        setup();
+        store.dispatch({
+            type: actionTypes.ADD_TERM,
+            field: 'field',
+            value: 'value'
+        });
+        store.dispatch({
+            type: actionTypes.ADD_TERM,
+            field: 'field',
+            value: 'value1'
+        });
+        await wait();
+        expect(store.getState().search.query.selection).toEqual({
+            field: ['value', 'value1']
+        });
+    });
 });
+
+function wait(ms?: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
