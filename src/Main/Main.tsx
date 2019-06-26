9b6efe21bd5e224015342f7c4637954338bdb288
RosMEW <rosalie.yue.liu@gmail.com>
2019-06-25 20:15:33 -0400

    Add error modal

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -4,16 +4,19 @@ import { useSelector } from 'react-redux';
 import Selection from './Selection/Selection';
 import Products from './Products/Products';
 import Loader from '../UI/Loader/Loader';
+import ErrorModal from '../UI/ErrorModal/ErrorModal';
 
 import { state } from '../shared/types';
 import './Main.scss';
 
 const Main = () => {
-    const loading = useSelector((state: state) => state.search.loading);
+    const { error, loading } = useSelector((state: state) => state.search);
 
     return (
         <main>
-            {loading ? <Loader /> : <Products />} <Selection />
+            {error ? <ErrorModal /> : null}
+            {loading ? <Loader /> : <Products />}
+            <Selection />
         </main>
     );
 };
